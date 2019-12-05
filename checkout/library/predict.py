6b55b863792b173265175bbbaf6bd15b2b4cf3ae
agnieszkajureczko <stonogunia567@gmail.com>
2019-12-05 20:45:22 +0100

    Deleted taken photos from images folder

--- checkout/library/predict.py
+++ checkout/library/predict.py
@@ -5,6 +5,12 @@ import io
 import keras
 import tensorflow as tf
 import keras.preprocessing.image
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+import keras.backend as K
+import cv2
+import sys
+import matplotlib.pyplot as plt
 from flask import request, jsonify, Flask, abort
 from tensorflow.python.keras.backend import set_session
 from flask import render_template
@@ -12,7 +18,6 @@ import argparse
 import os
 import operator
 import re
-import uuid
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 print (f"current path: {current_path}")
@@ -37,6 +42,45 @@ fruits = [{"fruit": "Jablko", "prediction": 0, "image": "static/images/apple.png
 def get_camera():
     return render_template("camera.html")
 
+def heatmap():
+    model = VGG16('/content/drive/My Drive/models/weights_model2611.h5')
+    img_path = '/content/drive/My Drive/Dane/singleTest/Tomato/20191017_195023.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    preds = model.predict(x)
+    class_idx = np.argmax(preds[0])
+    class_output = model.output[:, class_idx]
+    last_conv_layer = model.get_layer("block5_conv3")
+
+    grads = K.gradients(class_output, last_conv_layer.output)[0]
+    pooled_grads = K.mean(grads, axis=(0, 1, 2))
+    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+    pooled_grads_value, conv_layer_output_value = iterate([x])
+    for i in range(512):
+        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+    heatmap = np.mean(conv_layer_output_value, axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+
+    img = cv2.imread(img_path)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+
+    plt.figure(figsize=(12, 10))
+    plt.imshow(img)
+    plt.imshow(superimposed_img, cmap='viridis')
+    plt.grid(False)
+    plt.show()
+    plt.savefig('/content/drive/My Drive/Dane/foo.png')
+    cm = plt.get_cmap('viridis')
+    img = Image.fromarray(((superimposed_img)[:, :, :3] * 255).astype(np.uint8))
+    img.save('/content/drive/My Drive/Dane/image.png')
 
 def preprocess_image(image, target_size):
     image = image.resize(target_size)
@@ -51,6 +95,16 @@ def stringProcess(oldValue):
            oldValue = oldValue.replace(elem, '_')
     newString = oldValue + ".jpg"
     return newString
+
+def processProductsList(products):
+    for i in range(len(fruits)):
+        fruits[i]["prediction"] = round(products[0][i]*100,2)
+        # sortowanie listy predykcji
+    fruits.sort(key=operator.itemgetter('prediction'), reverse=True)
+    for i in range(len(fruits)):
+        list = [fruits[i]['fruit'], fruits[i]['prediction']]
+        print("class: {}, prediction: {}%".format(*list))
+    return fruits
 
 def get_image_from_request(request):
     message = request.get_json(force=True)
@@ -70,16 +124,6 @@ def get_image_from_request(request):
             return image, imageName
     else:
         raise BadRequestException('Nie wysłano żadnego pliku')
-
-def processProductsList(products):
-    for i in range(len(fruits)):
-        fruits[i]["prediction"] = round(products[0][i]*100,2)
-        # sortowanie listy predykcji
-    fruits.sort(key=operator.itemgetter('prediction'), reverse=True)
-    for i in range(len(fruits)):
-        list = [fruits[i]['fruit'], fruits[i]['prediction']]
-        print("class: {}, prediction: {}%".format(*list))
-    return fruits
 
 @app.route("/", methods=["POST"])
 def predict():
