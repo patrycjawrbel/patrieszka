98c6d38821acdb12fe243c855971f300772b8924
patrycjawrbel <patrycjawrbel@gmail.com>
2020-01-06 21:18:59 +0100

    heatmap added to project

--- checkout/checkout/predict.py
+++ checkout/checkout/predict.py
@@ -2,13 +2,14 @@ import keras
 import tensorflow as tf
 import keras.preprocessing.image
 from flask import request, Flask, abort
+from keras.applications.vgg16 import VGG16
 from tensorflow.python.keras.backend import set_session
 from flask import render_template, redirect, url_for
 import argparse
 import os
 from checkout.python.get__image import get_image_from_request
 from checkout.python.process_products_list import process_products_list
-from checkout.python.preprocess_image import preprocess_image
+from checkout.python.preprocess_image import preprocess_image, get_heatmap
 from checkout.database.connection import connect_database
 from checkout.database.insert_pred import insert_pred
 from checkout.database.select_class import select_class
@@ -16,35 +17,45 @@ from checkout.database.select_last import select_last
 from checkout.database.insert_label import insert_label
 
 current_path = os.path.dirname(os.path.abspath(__file__))
+
+
 class BadRequestException(Exception):
     pass
 
+
 app = Flask(__name__)
 
+
 def init_models():
-    #załadowanie argumentów wiesza poleceń
-    parser = argparse.ArgumentParser(description = "")
-    parser.add_argument("-host", '--host', help = "Host aplikacji - domyślny to 'localhost'", required = False, default = 'localhost')
-    parser.add_argument("-port", '--port', help = "Port for web app", required = False, default = 5000)
-    parser.add_argument("-paths_to_models", '--paths_to_models', help = "Ścieżka do plików modeli - domyślna to '../models'", required = False, default = current_path + "\\..\\models")
-    parser.add_argument("-paths_to_photos", '--paths_to_photos', help = "Scieżka gdzie mają być zapisywane przesyłane zdjęcia - domyślna to '../received_images'", required = False, default = current_path + '\\..\\' + 'received_images')
+    # załadowanie argumentów wiesza poleceń
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-host", '--host', help="Host aplikacji - domyślny to 'localhost'", required=False,
+                        default='localhost')
+    parser.add_argument("-port", '--port', help="Port for web app", required=False, default=5000)
+    parser.add_argument("-paths_to_models", '--paths_to_models',
+                        help="Ścieżka do plików modeli - domyślna to '../models'", required=False,
+                        default=current_path + "\\..\\models")
+    parser.add_argument("-paths_to_photos", '--paths_to_photos',
+                        help="Scieżka gdzie mają być zapisywane przesyłane zdjęcia - domyślna to '../received_images'",
+                        required=False, default=current_path + '\\..\\' + 'received_images')
     argument = parser.parse_args()
 
     global host, port, weights, conn
     host = argument.host
     port = argument.port
     print("**Loading model")
-    global model, graph, vgg16, session
+    global model, graph, vgg16, session, model_vgg
     session = tf.Session()
     set_session(session)
-   # models_path = argument.paths_to_models
+    # models_path = argument.paths_to_models
     model = tf.keras.models.load_model('models/fc_model1.h5')
+    model_vgg = VGG16('models/fc_model1.h5')
     weights = model.load_weights('models/model_grocery.h5')
     vgg16 = keras.applications.VGG16(include_top=False, weights='imagenet')
     graph = tf.get_default_graph()
     print("**MODEL LOADED")
 
-    #connection with database
+    # connection with database
     conn = connect_database()
     print("**DATABASE CONNECTED**")
 
@@ -55,9 +66,11 @@ def init_models():
         os.mkdir(paths_to_photos)
     return host, port
 
+
 @app.route('/', methods=["GET"])
 def get_camera():
     return render_template("camera.html")
+
 
 @app.route("/", methods=["POST"])
 def predict():
@@ -65,7 +78,7 @@ def predict():
     try:
         image, imageName = get_image_from_request(request)
         # Zapisanie przesłanego pliku
-        path = 'C:/Users/Agnieszka/Desktop/patrieszka/checkout/images/' + imageName
+        path = 'C:/Users/patry/Desktop/patrieszka/checkout/images/' + imageName
         image.save(path)
         processed_image = preprocess_image(image, target_size=(224, 224))
         prediction = None
@@ -75,6 +88,7 @@ def predict():
             prediction = model.predict(bt_prediction).tolist()
             insert_pred(conn, prediction, path)
             fruits = process_products_list(prediction)
+            heatmap_image_path = get_heatmap(path, model_vgg)
         return redirect(url_for('get_items'))
     except BadRequestException as error:
         abort(400, str(error))
@@ -82,25 +96,24 @@ def predict():
         raise error
         abort(500, str('Coś poszło nie tak po stronie serwera'))
 
+
 @app.route("/results")
 def get_items():
     return render_template("list.html", var=fruits)
+
 
 @app.route("/saveLabel", methods=['POST'])
 def ranking():
     message = request.get_json(force=True)
     labelName = message.get('fruitName', None)
     print(labelName)
+    # tutaj zapis do bazy!!!
     id_label = select_class(conn, labelName)
     id_current_pred = select_last(conn)
-    insert_label(conn,id_label,id_current_pred)
+    insert_label(conn, id_label, id_current_pred)
     return url_for("rank")
+
 
 @app.route("/ranking")
 def rank():
     return render_template("results.html", fruits=fruits)
-
-
-
-
-
--- checkout/checkout/python/preprocess_image.py
+++ checkout/checkout/python/preprocess_image.py
@@ -1,5 +1,11 @@
+import cv2
+import keras.backend as K
 import keras.preprocessing.image
+import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+from keras.applications.vgg16 import preprocess_input
+
 
 def preprocess_image(image, target_size):
     image = image.resize(target_size)
@@ -7,3 +13,39 @@ def preprocess_image(image, target_size):
     image = np.expand_dims(image, axis=0)
     image /= 255.
     return image
+
+
+def get_heatmap(path, model_vgg):
+    file_path = 'C:/Users/patry/Desktop/patrieszka/checkout/checkout/static/images/' + 'heatmap.jpg'
+    img = keras.preprocessing.image.load_img(path, target_size=(224, 224))
+    x = keras.preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    preds = model_vgg.predict(x)
+    class_idx = np.argmax(preds[0])
+    class_output = model_vgg.output[:, class_idx]
+    last_conv_layer = model_vgg.get_layer("block5_conv3")
+
+    grads = K.gradients(class_output, last_conv_layer.output)[0]
+    pooled_grads = K.mean(grads, axis=(0, 1, 2))
+    iterate = K.function([model_vgg.input], [pooled_grads, last_conv_layer.output[0]])
+    pooled_grads_value, conv_layer_output_value = iterate([x])
+    for i in range(512):
+        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+    heatmap = np.mean(conv_layer_output_value, axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+
+    img = cv2.imread(path)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+
+    cm = plt.get_cmap('viridis')
+    img = Image.fromarray(((superimposed_img)[:, :, :3] * 255).astype(np.uint8))
+    heatmap_path = file_path
+    img.save(heatmap_path)
+    return heatmap_path
